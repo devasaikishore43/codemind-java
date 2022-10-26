1ba29d9eea72049fad2361ce975c1c4d05c63f3c
T.Deva Sai Kishore <81376904+devasaikishore43@users.noreply.github.com>
2022-10-26 23:18:35 +0530

    import java.util.*; class Kishor{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int a,c,d,count=0;         a = sc.nextInt();         int b[]=new int[a];         for (int i=0;i<a;i++)         {b[i] = sc.nextInt();}         c = sc.nextInt();         d = sc.nextInt();         for (int i=0;i<a;i++)         {             if (c<=b[i] && d>=b[i]){                 System.out.print(b[i]+" ");                 count++;}
            }
            if (count==0){System.out.println(-1);}
        }
    }

---
+++ elements_in_between_A_and_B.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Kishor{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a,c,d,count=0;
+        a = sc.nextInt();
+        int b[]=new int[a];
+        for (int i=0;i<a;i++)
+        {b[i] = sc.nextInt();}
+        c = sc.nextInt();
+        d = sc.nextInt();
+        for (int i=0;i<a;i++)
+        {
+            if (c<=b[i] && d>=b[i]){
+                System.out.print(b[i]+" ");
+                count++;}
+            
+        }
+        if (count==0){System.out.println(-1);}
+    }
+}
