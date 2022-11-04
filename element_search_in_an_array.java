efa234c765fc3f8a19aa64a6c8f808eee94dfb6e
T.Deva Sai Kishore <81376904+devasaikishore43@users.noreply.github.com>
2022-11-04 19:20:12 +0530

    import java.util.*; class Kishore{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int count=0,b,c,d;         c= sc.nextInt();         int []arr=new int[c];         for (int i=0;i<c;i++){             arr[i] = sc.nextInt();         }         d = sc.nextInt();         for (int i=0;i<c;i++){             if (arr[i]==d){count++;}         }         if (count>0){System.out.println("True");}         else{             System.out.println("False");         }     } }

---
+++ element_search_in_an_array.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Kishore{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int count=0,b,c,d;
+        c= sc.nextInt();
+        int []arr=new int[c];
+        for (int i=0;i<c;i++){
+            arr[i] = sc.nextInt();
+        }
+        d = sc.nextInt();
+        for (int i=0;i<c;i++){
+            if (arr[i]==d){count++;}
+        }
+        if (count>0){System.out.println("True");}
+        else{
+            System.out.println("False");
+        }
+    }
+}
