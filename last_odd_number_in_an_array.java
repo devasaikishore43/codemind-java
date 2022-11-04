add5e0aff6c1dcc366ef38ea442a3565c036fe08
T.Deva Sai Kishore <81376904+devasaikishore43@users.noreply.github.com>
2022-11-04 19:14:16 +0530

    import java.util.*; class Kishore{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int count=0,b,c,d;         c= sc.nextInt();         for (int i=0;i<c;i++){             d = sc.nextInt();             if (d%2==1){count = d;}         }
            System.out.println(count);
        }
    }

---
+++ last_odd_number_in_an_array.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Kishore{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int count=0,b,c,d;
+        c= sc.nextInt();
+        for (int i=0;i<c;i++){
+            d = sc.nextInt();
+            if (d%2==1){count = d;}
+        }
+        
+        System.out.println(count);
+    }
+}
